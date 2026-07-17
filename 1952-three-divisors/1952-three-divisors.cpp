156700cfd654469ddcf1410646a77ae80edf9098
Barsha Mondal <barshadgp212@gmail.com>
2026-07-18 02:49:20 +0530

    Time: 0 ms (100%), Space: 7.8 MB (22.19%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int root = sqrt(n);
+
+        if (root * root != n)
+            return false;
+
+        for (int i = 2; i * i <= root; i++) {
+            if (root % i == 0)
+                return false;
+        }
+
+        return root > 1;
+    }
+};
