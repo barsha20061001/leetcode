dc4eb9997d721860cec1f66bcb4b2985447c6ac4
Barsha Mondal <barshadgp212@gmail.com>
2026-07-22 15:27:33 +0530

    Time: 0 ms (100%), Space: 14.3 MB (88.19%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<bool> visited(n, false);
+        stack<int> st;
+
+        st.push(0);
+        visited[0] = true;
+
+        while (!st.empty()) {
+            int room = st.top();
+            st.pop();
+
+            for (int key : rooms[room]) {
+                if (!visited[key]) {
+                    visited[key] = true;
+                    st.push(key);
+                }
+            }
+        }
+
+        for (bool roomVisited : visited) {
+            if (!roomVisited) return false;
+        }
+
+        return true;
+    }
+};
