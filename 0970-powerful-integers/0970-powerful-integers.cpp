b47b4bca2e893368790f03726fa3c777ae459a87
Barsha Mondal <barshadgp212@gmail.com>
2026-08-06 12:27:35 +0530

    Time: 3 ms (13.65%), Space: 9.3 MB (33.98%) - LeetHub

---
+++ 0970-powerful-integers/0970-powerful-integers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+vector<int> powerfulIntegers(int x, int y, int bound) {
+unordered_set<int> st;
+ for (long long a = 1; a <= bound; a *= x) {
+for (long long b = 1; a + b <= bound; b *= y) {
+st.insert(a + b);
+if (y == 1) break; }
+ if (x == 1) break;}
+return vector<int>(st.begin(), st.end()); }
+};
