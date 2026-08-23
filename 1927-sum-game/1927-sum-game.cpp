2ad0b583d2eaf1ec62a56e39582e518cf609f6f6
Barsha Mondal <barshadgp212@gmail.com>
2026-08-23 08:17:41 +0530

    Time: 3 ms (66.67%), Space: 13.9 MB (92.07%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            if (num[i] == '?')
+                leftQ++;
+            else
+                leftSum += num[i] - '0';
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            if (num[i] == '?')
+                rightQ++;
+            else
+                rightSum += num[i] - '0';
+        }
+
+        int qDiff = leftQ - rightQ;
+        int sumDiff = leftSum - rightSum;
+
+        // Odd difference in number of '?' -> Alice wins
+        if (abs(qDiff) % 2 == 1)
+            return true;
+
+        // Bob wins only if the differences can exactly cancel
+        if (sumDiff == -9 * qDiff / 2)
+            return false;
+
+        return true;
+    }
+};
